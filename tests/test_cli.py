cb16cb176b416bb71fc6c8d379f69cb6124d1696
Eran Kampf <eran@ekampf.com>
2016-06-03 09:51:49 -0700

    Fixed tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,7 +3,7 @@ from click.testing import CliRunner
 from graphql_schema_validator import cli
 import os.path
 
-SCHEMA = os.path.join(os.path.dirname(__file__), 'wondermall.schema')
+SCHEMA = os.path.join(os.path.dirname(__file__), 'starwars.schema')
 VALID_QUERY = os.path.join(os.path.dirname(__file__), 'valid.query')
 INVALID_QUERY = os.path.join(os.path.dirname(__file__), 'invalid.query')
 
@@ -17,15 +17,16 @@ def test_cli(runner):
     assert result.exception
 
 def test_cli_with_valid_query(runner):
-    result = runner.invoke(cli.main, ['-s %s' % SCHEMA, '-q %s' % VALID_QUERY])
+    result = runner.invoke(cli.main, ['-s', SCHEMA, '-q', VALID_QUERY])
     assert not result.exception
     assert result.exit_code == 0
-    assert result.output.strip() == 'Valid query.'
+    assert result.output.strip() == 'Valid query'
 
 
 def test_cli_with_invalid_query(runner):
-    result = runner.invoke(cli.main, ['-s %s' % SCHEMA, '-q %s' % INVALID_QUERY])
+    expected = 'Cannot query field "invalidProperty" on type "Faction".'
+
+    result = runner.invoke(cli.main, ['-s', SCHEMA, '-q', INVALID_QUERY])
     assert result.exit_code != 0
     assert result.exception
-    assert result.output.strip() == '''Error: 
-Cannot query field "users" on type "RelayRootType".'''
+    assert expected in result.output.strip()
